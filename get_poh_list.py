7d1245a27bc325a0a152cf7d9658f9f59f6985ef
nkymz <nakayama7322@gmail.com>
2018-10-21 21:22:20 +0900

    * deal with the change in POG_HorseList.xlsx

--- get_poh_list.py
+++ get_poh_list.py
@@ -15,10 +15,10 @@ def get_poh_list():
     wb = openpyxl.load_workbook(WBPATH)
     wshl = wb["POHorseList"]
 
-    trow = 1
-    while wshl.cell(row=trow, column=1).value:
-        horse_name = wshl.cell(row=trow, column=2).value
-        horse_name_origin = wshl.cell(row=trow, column=3).value
+    xlrow = 2
+    while wshl.cell(row=xlrow, column=1).value:
+        horse_name = wshl.cell(row=xlrow, column=2).value
+        horse_name_origin = wshl.cell(row=xlrow, column=3).value
 
         is_horse_name_determined = False
 
@@ -28,12 +28,12 @@ def get_poh_list():
             is_horse_name_determined = True
 
         if is_horse_name_determined and horse_name_origin:
-            trow += 1
+            xlrow += 1
             continue
 
-        horse_url_sp = wshl.cell(row=trow, column=5).value
+        horse_url_sp = wshl.cell(row=xlrow, column=5).value
         if horse_url_sp is None:
-            trow += 1
+            xlrow += 1
             continue
 
         time.sleep(1)
@@ -43,15 +43,15 @@ def get_poh_list():
         horse_name_new = soup.find("p", class_="Name").string
         horse_name_origin_new = soup.find("th", string="馬名の意味").find_next().string
 
-        wshl.cell(row=trow, column=2).value = horse_name_new
+        wshl.cell(row=xlrow, column=2).value = horse_name_new
         if horse_name_origin_new != "-":
-            wshl.cell(row=trow, column=3).value = horse_name_origin_new
+            wshl.cell(row=xlrow, column=3).value = horse_name_origin_new
 
-        trow += 1
+        xlrow += 1
 
     wb.save(WBPATH)
 
-    return [[cell.value for cell in row] for row in wshl["A1:F" + str(trow - 1)]]
+    return [[cell.value for cell in row] for row in wshl["A2:F" + str(xlrow - 1)]]
 
 
 if __name__ == "__main__":
